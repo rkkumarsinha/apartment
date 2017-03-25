5bc6324397d8e60f3dede7631e49baf338e21c9b
sinharakesh <rksinha.btech@gmail.com>
2017-03-25 07:49:37 +0530

    included in vendor

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -6,6 +6,11 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'EasyPeasyICS' => $vendorDir . '/phpmailer/phpmailer/extras/EasyPeasyICS.php',
+    'PHPMailer' => $vendorDir . '/phpmailer/phpmailer/class.phpmailer.php',
+    'PHPMailerOAuth' => $vendorDir . '/phpmailer/phpmailer/class.phpmaileroauth.php',
+    'PHPMailerOAuthGoogle' => $vendorDir . '/phpmailer/phpmailer/class.phpmaileroauthgoogle.php',
+    'POP3' => $vendorDir . '/phpmailer/phpmailer/class.pop3.php',
     'Pest' => $vendorDir . '/educoder/pest/Pest.php',
     'PestJSON' => $vendorDir . '/educoder/pest/PestJSON.php',
     'PestXML' => $vendorDir . '/educoder/pest/PestXML.php',
@@ -27,4 +32,7 @@ return array(
     'Pest_ServerError' => $vendorDir . '/educoder/pest/Pest.php',
     'Pest_Unauthorized' => $vendorDir . '/educoder/pest/Pest.php',
     'Pest_UnknownResponse' => $vendorDir . '/educoder/pest/Pest.php',
+    'SMTP' => $vendorDir . '/phpmailer/phpmailer/class.smtp.php',
+    'ntlm_sasl_client_class' => $vendorDir . '/phpmailer/phpmailer/extras/ntlm_sasl_client.php',
+    'phpmailerException' => $vendorDir . '/phpmailer/phpmailer/class.phpmailer.php',
 );
